098af6d0070287323c4b1e435b3483294522c072
Jean-Francois Turcot <jf.turcot@gmail.com>
2013-08-29 13:29:37 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TorqueboxRailsSample::Application.config.secret_key_base = 'cba37a02f2d2a2a88706fe27abd3f520d399f85e6733d2e2b7766d9713fd0be88106e740f8398f700ffc5f5f9aa60389763f728b40c26367b460c23c536ba8b5'
